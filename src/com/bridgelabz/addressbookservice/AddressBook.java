c0c75642fd5dc1862c6854c21a904d21d50c73ba
Rohithv17 <rohithv1396@gmail.com>
2024-02-19 00:34:56 +0530

    Added feature to edit contact

--- src/com/bridgelabz/addressbookservice/AddressBook.java
+++ src/com/bridgelabz/addressbookservice/AddressBook.java
@@ -6,25 +6,25 @@ import java.util.List;
 
 public class AddressBook {
 
-        private List<Contacts> contacts;
+    private List<Contacts> contacts;
 
-        public AddressBook() {
-            contacts = new ArrayList<>();
-        }
+    public AddressBook() {
+        contacts = new ArrayList<>();
+    }
 
-        public void addContact(Contacts contact) {
-            contacts.add(contact);
-        }
+    public void addContact(Contacts contact) {
+        contacts.add(contact);
+    }
 
-        public void displayContacts() {
-            if (contacts.isEmpty()) {
-                System.out.println("Address book is empty!");
-            } else {
-                for (Contacts contact : contacts) {
-                    System.out.println(contact);
-                }
+    public void displayContacts() {
+        if (contacts.isEmpty()) {
+            System.out.println("Address book is empty!");
+        } else {
+            for (Contacts contact : contacts) {
+                System.out.println(contact);
             }
         }
+    }
 
     public Contacts findContactByName(String firstName, String lastName) {
         for (Contacts contact : contacts) {
@@ -34,7 +34,8 @@ public class AddressBook {
         }
         return null; // Contact not found
     }
-//  to edit contacts using first and last name
+
+    //  to edit contacts
     public void editContact(Contacts contact, String newFirstName, String newLastName, String newAddress,
                             String newCity, String newState, String newZip, String newPhoneNumber, String newEmail) {
         contact.setFirstName(newFirstName);
@@ -46,18 +47,7 @@ public class AddressBook {
         contact.setPhoneNumber(newPhoneNumber);
         contact.setEmail(newEmail);
     }
-    public void deleteContact(String firstName, String lastName) { // To delete contact using first name and last name
-        Iterator<Contacts> iterator = contacts.iterator();
-        while (iterator.hasNext()) {
-            Contacts contact = iterator.next();
-            if (contact.getFirstName().equals(firstName) && contact.getLastName().equals(lastName)) {
-                iterator.remove();
-                System.out.println("Contact deleted successfully!\n");
-                return;
-            }
-        }
-        System.out.println("Contact not found.\n");
-    }
+
 }
 
 
